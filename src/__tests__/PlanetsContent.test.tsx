5988e3185d26282a0cb0b987801fc9bb0df2291b
Jaromir Pirko <jpirko@chemaxon.com>
2019-09-17 17:36:39 +0200

    jarouch-enzyme-jest fix prettier and eslint

--- src/__tests__/PlanetsContent.test.tsx
+++ src/__tests__/PlanetsContent.test.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import PlanetsContent from '../components/pages/PlanetsContent';
+import React from 'react'
+import { shallow } from 'enzyme'
+import PlanetsContent from '../components/pages/PlanetsContent'
 describe('PlanetsContent', () => {
-    it('should render correctly in "debug" mode', () => {
-        const component = shallow(<PlanetsContent />);
+  it('should render correctly in "debug" mode', () => {
+    const component = shallow(<PlanetsContent />)
 
-        expect(component).toMatchSnapshot();
-    });
-});
+    expect(component).toMatchSnapshot()
+  })
+})
