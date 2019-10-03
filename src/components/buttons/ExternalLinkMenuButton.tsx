e9a6779768d8f0e39ee8de84629e63d680343ea4
Martin Krauskopf <martin.krauskopf@gmail.com>
2019-10-03 21:28:38 +0200

    Introduce separate components for external link and route buttons.

---
+++ src/components/buttons/ExternalLinkMenuButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import * as S from './styles'
+
+interface IOwnProps {
+  link: string
+  children: React.ReactNode
+}
+
+const ExternalLinkMenuButton = (props: IOwnProps) => (
+  <S.MenuButton variant='text' href={props.link} target='_blank'>
+    {props.children}
+  </S.MenuButton>
+)
+
+export default ExternalLinkMenuButton
---
+++ src/components/buttons/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import { Button } from '@material-ui/core'
+import { ExtendButtonBase } from '@material-ui/core/ButtonBase'
+import { ButtonTypeMap } from '@material-ui/core/Button'
+
+export const MenuButton: ExtendButtonBase<ButtonTypeMap> = styled(Button).attrs({ className: 'DS-MenuButton' })`
+  && {
+    color: white;
+  }
+`
