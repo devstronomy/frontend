bfc833c18f1eb04b4e42e1e0c7d2c22238ec6793
Martin Krauskopf <martin.krauskopf@gmail.com>
2019-11-01 08:43:11 +0100

    Make Solar System table main page. Move the former one to About section.

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../App'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import appReducers from '../components/reducer'
 
 it('renders without crashing', () => {
+  const store = createStore(appReducers)
+  const app = (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(app, div)
   ReactDOM.unmountComponentAtNode(div)
 })
