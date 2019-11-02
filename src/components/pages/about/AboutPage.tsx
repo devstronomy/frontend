9d1b56af56006fbece806802a84f10703c1bd6be
Martin Krauskopf <martin.krauskopf@gmail.com>
2019-11-02 17:35:26 +0100

    Isolate AboutPage component.

---
+++ src/components/pages/about/AboutPage.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Links from '../../../links'
+import * as S from './styles'
+
+/** Represents the 'About' page of Devstronomy application. */
+const AboutPage = () => (
+  <S.AboutPage>
+    <h1>Datasets</h1>
+    <p>
+      Devstronomy project provides <Link to='/datasets'>datasets</Link> related to astronomy in an accessible format
+      (CSV, JSON, SQL). Currently, datasets of planets of our solar system and their natural satellites by {Links.jpl}
+      &nbsp; are available.
+    </p>
+
+    <h1>Interactive Demo</h1>
+    <p>
+      Apart from the datasets themselves, there is an <Link to='/planets'>interactive demo</Link> of the data.
+    </p>
+  </S.AboutPage>
+)
+
+export default AboutPage
---
+++ src/components/pages/about/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import { Page } from '../../styles'
+
+export const AboutPage = styled(Page)`
+  && {
+    padding: 20px 20px 0 20px;
+    font-size: 1.1em;
+  }
+`
