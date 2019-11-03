ab094048222a15c616cff72c0fedd3aa1636bbd7
Martin Krauskopf <martin.krauskopf@gmail.com>
2019-11-03 11:06:08 +0100

    Move 'Page' styled component to 'pages' directory.

--- src/components/pages/about/styles.ts
+++ src/components/pages/about/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { Page } from '../../styles'
+import { Page } from '../styles'
 
 export const AboutPage = styled(Page)`
   && {
--- src/components/pages/datasets/styles.ts
+++ src/components/pages/datasets/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { Page } from '../../styles'
+import { Page } from '../styles'
 
 export const DatasetsPage = styled(Page)``
 
