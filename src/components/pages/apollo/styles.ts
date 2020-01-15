34d71bfeccc9d7a04766324a97d2a0608081df26
Martin Krauskopf <martin.krauskopf@gmail.com>
2020-01-15 17:59:20 +0100

    Search field based on Material UI.
    With a few flaws. To be improved.

--- src/components/pages/apollo/styles.ts
+++ src/components/pages/apollo/styles.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import { TextField } from '@material-ui/core'
+import { dsBlue, dsBlueLight } from '../../globalStyles'
 
 export const ApolloPage = styled.div`
   min-height: 200px;
@@ -6,4 +8,26 @@ export const ApolloPage = styled.div`
   max-width: 800px;
   padding: 20px 20px 0 20px;
   flex-grow: 1;
+`
+
+export const DSTextField = styled(TextField)`
+  && {
+    margin: 0 0 5px 0;
+  }
+
+  .MuiInputLabel-root {
+    color: ${dsBlueLight};
+  }
+
+  .MuiOutlinedInput-input {
+    padding: 14px 10px;
+  }
+
+  .MuiInputLabel-root.Mui-focused {
+    color: ${dsBlue};
+  }
+
+  .MuiOutlinedInput-notchedOutline {
+    border-color: ${dsBlueLight};
+  }
 `
