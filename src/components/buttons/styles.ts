69624b3d80605165b5ffc78485efe5dc926467f3
Martin Krauskopf <martin.krauskopf@gmail.com>
2020-10-30 18:23:31 +0100

    Better responsiveness for the menu buttons.

--- src/components/buttons/styles.ts
+++ src/components/buttons/styles.ts
@@ -3,8 +3,14 @@ import { ButtonTypeMap } from '@material-ui/core/Button'
 import { ExtendButtonBase } from '@material-ui/core/ButtonBase'
 import styled from 'styled-components'
 
+const widthBoundary = '600px'
+
 export const MenuButton: ExtendButtonBase<ButtonTypeMap> = styled(Button).attrs({ className: 'DS-MenuButton' })`
   && {
+    @media (max-width: ${widthBoundary}) {
+      font-size: 0.7rem;
+    }
+
     color: white;
     border-bottom: none;
   }
